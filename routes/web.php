423107436d0357e75319cc9628e6446f1878c928
Jawad Nasar <jawadnasar7886@gmail.com>
2026-03-11 16:19:09 +0000

    extraction of flight numbers done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,13 +15,14 @@ Route::post('/login', [AuthController::class, 'store'])->name('login.store');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', function () {
-        return 'Welcome to dashboard, ';
-    })->name('dashboard');
-
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    
+    // Flight checking routes
+    Route::get('/check-flights', [App\Http\Controllers\CheckFlightsTimeController::class, 'show'])->name('check_flights');
+    Route::post('/check-flights', [App\Http\Controllers\CheckFlightsTimeController::class, 'checkFlights'])->name('check_flights.submit');
 });
 
-Route::get('all_airport_parking_prices', [App\Http\Controllers\AllAirportParkingPrices::class, 'index']);
+Route::get('all_airport_parking_prices', [App\Http\Controllers\AllAirportParkingPrices::class, 'index'])->name('all_airport_parking_prices');
 Route::post('all_airport_parking_prices/search_by_dates', [App\Http\Controllers\AllAirportParkingPrices::class, 'search_by_dates'])->name('all_airport_parking_prices.search_by_dates');
 Route::post('all_airport_parking_prices/download_excel', [App\Http\Controllers\AllAirportParkingPrices::class, 'download_excel'])->name('all_airport_parking_prices.download_excel');
 
